03a7cbec13d9af39bc55c6456c1cb077342760ab
mbtobin <tobin.matthew94@gmail.com>
2022-11-12 14:24:47 -0800

    moved html file to the root folder to prep for deployment and saved gif of working demo

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 //All of the code inside the following function will run when the page is loaded. 
 $(function () {
   // Gets the current date and adds it to the header.
-  var today = dayjs().format('dddd, MMMM DD');
+  var today = dayjs().format('dddd, MMMM DD, YYYY');
   $('#currentDay').text(today);
 
   // Click event for the save button to save our calendar events.  
@@ -16,7 +12,7 @@ $(function () {
   });
   
   // Set an array to be able to call on the keys created in our setItem function
-  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
   
   // This for loop will loop through each key used to save the calendar events and keep them on the calendar
   for (var i = 0; i < workHours.length; i ++) {
@@ -25,10 +21,14 @@ $(function () {
     hourTextContent.textContent = eventFromStorage;
   }
 
-  // Sets a variable that gets the current hour of the day.
+  // Sets a variable that gets the current hour of the day using DayJS.
   var realTimeHour = dayjs().hour();
   console.log(realTimeHour);
   
+  // This for loop compares the time block hour of the calendar event vs the real time hour.
+  // If time block hour is less than current hour then the time block will be gray.
+  // If time block hour matches the current hour then the time block gets the style of present with a red background.
+  // If time block hour is greater than current hour then it will be styled for future with a green background.
   for (var i = 0; i < workHours.length; i ++) {
     if (workHours[i] < realTimeHour) {
       $('#hour-' + workHours[i]).addClass('past');
@@ -41,22 +41,4 @@ $(function () {
       $('#hour-' + workHours[i]).addClass('future');
     }
   }
-
-  // This code should use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
